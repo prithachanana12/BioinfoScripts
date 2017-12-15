09426a5eae116f85d8253a82994d6684b4fb96f4
Pritha  Chanana;Chanana.Pritha@mayo.edu;16134575 <m133293@franklin05.mayo.edu>
2017-12-15 15:15:35 -0600

    stringtie job submission & log keeping edits

--- mRNA/expr_quant/stringtie.sh
+++ mRNA/expr_quant/stringtie.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+exec &> $4/logfile.txt
 if [ $# != 4 ]
 then
 	echo -e "Usage: Run StringTie in batch mode\n StringTie.sh <path to BAM> <samples (colon separated)> <reference annotation file> <path to output directory>"
@@ -10,7 +11,6 @@ else
 	sample=$2
 	ref_gtf=$3
 	out_dir=$4
-	
 
 	stringtie=/data5/bsi/bictools/src/stringtie/1.3.3/
 	email=`finger $USER | awk -F ';' '{print $2}' | head -n1`
@@ -18,16 +18,17 @@ else
 	mkdir -p $logs
 	hold_jobs=''
 	for i in `echo $sample | tr ':' ' '`; do
-	qsub -N string_pass1_${i} -m ae -M $email -q 1-day -l h_vmem=5G -l h_stack=10M -wd $logs -b y $stringtie/stringtie $bam/${i}/${i}.Aligned.sortedByCoord.out.bam -G $ref_gtf -l $i -o $out_dir/${i}.gtf
+	mkdir -p $out_dir/${i}
+	qsub -N string_pass1_${i} -m a -M $email -q 1-day -l h_vmem=5G -l h_stack=10M -wd $logs -b y $stringtie/stringtie $bam/${i}/${i}.Aligned.sortedByCoord.out.bam -G $ref_gtf -l $i -o $out_dir/${i}/${i}.gtf
 	hold_jobs+="string_pass1_${i},"
-	echo $out_dir/${i}.gtf >> $out_dir/mergelist.txt
+	echo $out_dir/${i}/${i}.gtf >> $out_dir/mergelist.txt
 	done
 
-	qsub -N string_merge -m ae -M $email -hold_jid ${hold_jobs%?} -q 1-day -l h_vmem=5G -l h_stack=10M -wd $logs -b y $stringtie/stringtie --merge -G $ref_gtf -o $out_dir/stringtie_merged.gtf $out_dir/mergelist.txt
+	job_id=$(qsub -N string_merge -m a -M $email -hold_jid ${hold_jobs%?} -q 1-day -l h_vmem=5G -l h_stack=10M -wd $logs -terse -b y $stringtie/stringtie --merge -G $ref_gtf -o $out_dir/stringtie_merged.gtf $out_dir/mergelist.txt)
 	
 	for i in `echo $sample | tr ':' ' '`; do 
-	qsub -N string_pass2_${i} -m ae -M $email -wd $logs -q 1-day -l h_vmem=5G -l h_stack=10M -hold_jid string_merge -b y $stringtie/stringtie $bam/${i}/${i}.Aligned.sortedByCoord.out.bam -G $out_dir/stringtie_merged.gtf -e -B -l $i -o $out_dir/${i}_mergedTrans.gtf
+	qsub -N string_pass2_${i} -m a -M $email -wd $logs -q 1-day -l h_vmem=5G -l h_stack=10M -hold_jid $job_id -b y $stringtie/stringtie $bam/${i}/${i}.Aligned.sortedByCoord.out.bam -G $out_dir/stringtie_merged.gtf -e -B -l $i -o $out_dir/${i}/${i}_mergedTrans.gtf
 	done
 
-    echo `date`
+
 fi
